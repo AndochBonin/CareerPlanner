12aec04bcc898452b5cc82b916e4d9e873e0dd75
kasimibrahim <kasim.ibrahim@ashesi.edu.gh>
2022-07-07 09:35:50 +0000

    Set up all the components

--- Frontend/Career-Planner/src/app/app.module.ts
+++ Frontend/Career-Planner/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeadingComponent } from './components/heading/heading.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadingComponent,
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
