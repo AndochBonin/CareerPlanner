ff61da010af4710d7203acbfb6cc17429de2127c
andoch <andoch.bonin@turntabl.io>
2022-07-07 13:03:29 -0700

    updated database to match springboot implementation

--- Database/CareerPlannerSchema.sql
+++ Database/CareerPlannerSchema.sql
@@ -1,12 +1,13 @@
---drop schema CareerPlanner cascade;
 
-create schema if not exists CareerPlanner;
 
---drop table if exists CareerPlanner.Students;
---drop table if exists CareerPlanner.Careers;
---drop table if exists CareerPlanner.Responses;
+--create database careerdb;
 
-create table CareerPlanner.Students 
+
+--drop table if exists Students;
+--drop table if exists Careers;
+--drop table if exists Responses;
+
+create table Students 
 (
 
 	student_id integer not null primary key,
@@ -16,7 +17,7 @@ create table CareerPlanner.Students
 	account_password varchar(40) not null
 );
 
-create table CareerPlanner.Careers 
+create table Careers 
 (
 	
 	career_id integer not null primary key,
@@ -25,7 +26,7 @@ create table CareerPlanner.Careers
 	avg_salary integer not null
 );
 
-create table CareerPlanner.Responses 
+create table Responses 
 (
 	
 	response_id integer not null primary key,
@@ -40,24 +41,21 @@ create table CareerPlanner.Responses
 	response9 varchar(255) not null,
 	response10 varchar(255) not null,
 	student_id integer not null,
-	foreign key (student_id) references CareerPlanner.Students(student_id)
+	foreign key (student_id) references Students(student_id)
 );
 
-delete from CareerPlanner.Careers;
+delete from Careers;
 
-insert into CareerPlanner.Careers 
+insert into Careers 
 values(1, 'Frontend', 
 'Front end engineers plan, design, build, and implement the user interface systems of websites, 
 software programs, and web-based applications.', 103830);
 
-insert into CareerPlanner.Careers 
+insert into Careers 
 values(2, 'Backend', 
 'Back End Engineers are responsible for curating the structure of server side information, 
 primarily writing scripts and APIs to be used by front end and UX engineers', 117366 );
 
-insert into CareerPlanner.Careers 
+insert into Careers 
 values(3, 'Full Stack', 'Full-stack engineers are computer programmers with proficiency in front and back-end systems. 
 They design user interactions on websites and develop servers and databases for website functionality', 105186 );
-
-
-
